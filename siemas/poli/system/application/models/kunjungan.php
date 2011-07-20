cbe7de8ad06fc0bf7089c1604cbf1a345a80d3d0
meri.marlina20 <meri.marlina20@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-07-20 13:31:18 +0000

    Impor pertama

---
+++ siemas/poli/system/application/models/kunjungan.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+Class Kunjungan extends Model{
+    function Kunjungan()
+    {
+        parent::Model;
+    }
+    
+    function get_kunjungan(){
+        
+        
+    }
+    
+}
+?>
