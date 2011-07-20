55c09e991a074f8338f3934d39bcf5b58612b28b
muhammad.abrari <muhammad.abrari@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-07-20 04:46:16 +0000

    PERTAMAX

---
+++ siemas/simpeg/forms/input_kegiatan.php
@@ -0,0 +1,26 @@
+<table width="100%" border="0">
+    <tbody>
+        <tr>
+            <td>Pilih pegawai</td>
+            <td>
+
+                <select name="id_pegawai">
+                    <option value="">Pilih:</option>
+                </select>
+
+            </td>
+        </tr>
+        <tr>
+            <td>Tanggal</td>
+            <td><input type="text" name="tanggal_kegiatan" maxlength="255" class="datepicker"/></td>
+        </tr>
+        <tr>
+            <td>Lokasi</td>
+            <td><input type="text" name="lokasi_kegiatan" maxlength="255"/></td>
+        </tr>
+        <tr>
+            <td>Kegiatan</td>
+            <td><textarea cols="30" rows="3" name="kegiatan"></textarea></td>
+        </tr>
+    </tbody>
+</table>
---
+++ siemas/simpeg/forms/input_struktur_organisasi.php
@@ -0,0 +1,24 @@
+<table width="100%">
+    <thead>
+        <tr>
+            <td>No</td>
+            <td>NIP</td>
+            <td>Nama</td>
+            <td>Jabatan</td>
+            <td>Atasan</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>123456789678</td>
+            <td>dr. Fulan</td>
+            <td>Dokter Umum</td>
+            <td>
+                <select name="atasan[]">
+                    <option value="0">-</option>
+                </select>
+            </td>
+        </tr>
+    </tbody>
+</table>
