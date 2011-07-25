66f84b67dc615b1d1d8f07d37152b777bba097a8
muhammad.abrari <muhammad.abrari@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-07-25 09:05:11 +0000

    CLEANUP : poli & loket

---
+++ siemas/simpeg/laporan/nilai_tpp.php
@@ -0,0 +1,114 @@
+<?php include 'header.php'; ?>
+
+<?php include '../forms/list_pegawai.php'; ?>
+
+<?php
+$bulan = array(
+    "",
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"
+);
+
+$bulan_ini = intval(date("n"));
+
+$tahun_ini = intval(date("Y"));
+$tahun = array($tahun_ini - 2, $tahun_ini - 1, $tahun_ini);
+
+$jumlah_hari_bulan_ini = cal_days_in_month(CAL_GREGORIAN, $bulan_ini, $tahun_ini);
+
+$tanggal_libur_bulan_ini = array(3,10,17,24,31);
+$tanggal_libur_bp_pemda_bulan_ini = array(3,10,17,24,31,2,9,16,23,30);
+
+?>
+
+
+<div class="belowribbon">
+    <h1>
+        Nilai TPP
+    </h1>
+</div>
+
+
+<div id="page" style="margin-top: 0px;">
+    <div style="margin: 0px 1%">
+
+        <div class="module">
+            <h2><span>Pilihan</span></h2>
+            <div class="module-body">
+
+                <p>Tahun
+                    <select id="tahun">
+                        <?php for ($i = 0; $i < count($tahun); $i++) : ?>
+                            <option value="<?php echo $tahun[$i]; ?>" <?php if ($tahun[$i] == $tahun_ini) echo 'selected="selected"'; ?>><?php echo $tahun[$i]; ?></option>
+                        <?php endfor; ?>
+                    </select>
+                   Bulan
+                    <select id="bulan">
+                        <?php for ($i = 1; $i <= 12; $i++) : ?>
+                            <option value="<?php echo $i; ?>" <?php if ($i == $bulan_ini) echo 'selected="selected"'; ?>><?php echo $bulan[$i]; ?></option>
+                        <?php endfor; ?>
+                    </select>
+                   <input type="button" value="Tampilkan" class="submit-green"/>
+
+            </div>
+        </div>
+
+
+        <div class="module">
+            <h2><span>Daftar nilai DP3</span></h2>
+            <div class="module-table-body">
+                <table width="100%">
+                    <thead>
+                        <tr>
+                            <th style="width: 10px;">No</th>
+                            <th style="width: 150px">Nama</th>
+                            <th style="width: 150px">Jabatan</th>
+                            <th>Jumlah kehadiran ideal</th>
+                            <th>Jumlah kehadiran dicapai</th>
+                            <th style="width: 100px">Nilai</th>
+                            <th>Jam kerja efektif ideal</th>
+                            <th>Jam kerja efektif dicapai</th>
+                            <th style="width: 100px">Nilai</th>
+                            <th>Jumlah apel ideal</th>
+                            <th>Jumlah apel dicapai</th>
+                            <th style="width: 100px">Nilai</th>
+                            <th style="width: 100px">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php for ($i = 1; $i < count($pegawai); $i++) : ?>
+                            <tr <?php if($i%2 == 0) echo 'class="even"' ?>>
+                                <td><?php echo $i; ?></td>
+                                <td><?php echo $pegawai[$i]; ?></td>
+                                <td>Dokter</td>
+                                <td>80</td>
+                                <td>80</td>
+                                <td>80</td>
+                                <td>80</td>
+                                <td>80</td>
+                                <td>80</td>
+                                <td>80</td>
+                                <td>80</td>
+                                <td>80</td>
+                                <td>80</td>
+                            </tr>
+                        <?php endfor; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
