9b93e2d6b4d054b617a508d274a94ea42e3557ba
muhammad.abrari <muhammad.abrari@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-07-25 04:54:49 +0000

    Commit-in loket & home punya ichanastasia - yang kedua~

---
+++ siemas/loket/L_pembayaran.php
@@ -0,0 +1,125 @@
+<script type="text/javascript">
+<!--
+function MM_jumpMenu(targ,selObj,restore){ //v3.0
+  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+  if (restore) selObj.selectedIndex=0;
+}
+//-->
+</script>
+<table width="383" border="0">
+  <tr>
+    <td width="137">Nama</td>
+    <td width="16">:</td>
+    <td width="216"><input type="text" name="textfield" id="textfield" /></td>
+  </tr>
+  <tr>
+    <td>Poli</td>
+    <td>:</td>
+    <td><form name="form" id="form">
+      <select name="jumpMenu" id="jumpMenu">
+        <option>GIGI</option>
+        <option>KIA</option>
+        <option>LAB</option>
+        <option>RADIOLOGI</option>
+            </select>
+    </form>    </td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td><div align="center">
+      <input type="submit" name="button2" id="button2" value="Cari" />
+    </div></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p>Total Pasien: <strong>5 orang</strong></p>
+<table width="889" border="0">
+  <tr>
+    <td width="40"><div align="center"><strong>No.</strong></div></td>
+    <td width="135"><div align="center"><strong>Nama</strong></div></td>
+    <td width="87"><div align="center"><strong>Umur</strong></div></td>
+    <td width="133"><div align="center"><strong>Alamat</strong></div></td>
+    <td width="72"><div align="center"><strong>Poli</strong></div></td>
+    <td colspan="3"><div align="center"><strong>Total Harga</strong></div></td>
+    <td width="144"><div align="center"><strong>Status Pembayaran</strong></div></td>
+  </tr>
+  <tr>
+    <td><div align="center">1</div></td>
+    <td>Meri Marlina</td>
+    <td>19 tahun</td>
+    <td>Cibogor</td>
+    <td>Gigi</td>
+    <td width="35"><div align="right">Rp
+        
+    </div></td>
+    <td width="61"><div align="right">15.000 </div></td>
+    <td width="144"><div align="center">
+      <input type="submit" name="button" id="button" value="Rincian" />
+    </div></td>
+    <td><form id="form1" name="form1" method="post" action="">
+      <input type="checkbox" name="checkbox" id="checkbox" />
+    Lunas
+    </form></td>
+  </tr>
+  <tr>
+    <td><div align="center">2</div></td>
+    <td>Meri Marlina</td>
+    <td>20 tahun</td>
+    <td>Pabaton</td>
+    <td>KIA</td>
+    <td><div align="right">Rp </div></td>
+    <td><div align="right">3.000 </div></td>
+    <td><div align="center">
+      <input type="submit" name="button3" id="button3" value="Rincian" />
+    </div></td>
+    <td><input type="checkbox" name="checkbox2" id="checkbox2" />
+Lunas </td>
+  </tr>
+  <tr>
+    <td><div align="center">3</div></td>
+    <td>Meri Marlina</td>
+    <td>35 tahun</td>
+    <td>Kedung Halah</td>
+    <td>KIA</td>
+    <td><div align="right">Rp 
+        
+    </div></td>
+    <td><div align="right">60.000</div></td>
+    <td><div align="center">
+      <input type="submit" name="button4" id="button4" value="Rincian" />
+    </div></td>
+    <td><input type="checkbox" name="checkbox3" id="checkbox3" />
+Lunas </td>
+  </tr>
+  <tr>
+    <td><div align="center">4</div></td>
+    <td>Meri Marlina</td>
+    <td>38 tahun</td>
+    <td>Pabaton</td>
+    <td>Umum</td>
+    <td><div align="right">Rp </div></td>
+    <td><div align="right">75.000 </div></td>
+    <td><div align="center">
+      <input type="submit" name="button5" id="button5" value="Rincian" />
+    </div></td>
+    <td><input type="checkbox" name="checkbox4" id="checkbox4" />
+Lunas </td>
+  </tr>
+  <tr>
+    <td><div align="center">5</div></td>
+    <td>Meri Marlina</td>
+    <td>29 tahun</td>
+    <td>Kedung Halah</td>
+    <td>Umum</td>
+    <td><div align="right">Rp 
+        
+    </div></td>
+    <td><div align="right">12.000</div></td>
+    <td><div align="center">
+      <input type="submit" name="button6" id="button6" value="Rincian" />
+    </div></td>
+    <td><input type="checkbox" name="checkbox5" id="checkbox5" />
+Lunas </td>
+  </tr>
+</table>
