972af215c0e64c9de1fac24a5f8b274737d93dfe
annisa.anastasia <annisa.anastasia@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-07-26 04:11:23 +0000

    commit ketiga

--- siemas/loket/header.php
+++ siemas/loket/header.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8;charset=utf-8" />
-		<title>Puskesmas Bogor Tengah</title>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8;charset=utf-8" />
+        <title>Puskesmas Bogor Tengah</title>
 
         <!-- CSS Reset -->
-		<link rel="stylesheet" type="text/css" href="Template_files/reset000.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="Template_files/reset000.css" media="screen" />
 
         <!-- Fluid 960 Grid System - CSS framework -->
-		<link rel="stylesheet" type="text/css" href="Template_files/grid0000.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="Template_files/grid0000.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="Template_files/colorbox.css" />
+        <link rel="stylesheet" type="text/css" href="css/redmond/jquery-ui-1.8.14.custom.css" />
+
 
         <!-- IE Hacks for the Fluid 960 Grid System -->
         <!--[if IE 6]><link rel="stylesheet" type="text/css" href="css/ie6.css" media="screen" /><![endif]-->
-		<!--[if IE 7]><link rel="stylesheet" type="text/css" href="css/ie.css" media="screen" /><![endif]-->
+        <!--[if IE 7]><link rel="stylesheet" type="text/css" href="css/ie.css" media="screen" /><![endif]-->
 
         <!-- Main stylesheet -->
         <link rel="stylesheet" type="text/css" href="Template_files/styles00.css" media="screen" />
@@ -22,6 +25,7 @@
 
         <!-- Table sorter stylesheet -->
         <link rel="stylesheet" type="text/css" href="Template_files/tablesor.css" media="screen" />
+
 
         <!-- Thickbox stylesheet -->
         <link rel="stylesheet" type="text/css" href="Template_files/thickbox.css" media="screen" />
@@ -33,136 +37,125 @@
         <!--<link rel="stylesheet" type="text/css" href="css/theme-green.css" media="screen" />-->
         <!--<link rel="stylesheet" type="text/css" href="css/theme-graphite.css" media="screen" />-->
 
-		<!-- JQuery engine script-->
-		<script type="text/javascript" src="Template_files/jquery-1.js"></script>
+        <!-- JQuery engine script-->
 
-		<!-- JQuery WYSIWYG plugin script -->
-		<script type="text/javascript" src="Template_files/jquery00.js"></script>
+        <script type="text/javascript" src="Template_files/jquery-1.js"></script>
+
+        <!-- JQuery WYSIWYG plugin script -->
+        <script type="text/javascript" src="Template_files/jquery00.js"></script>
 
         <!-- JQuery tablesorter plugin script-->
-		<script type="text/javascript" src="Template_files/jquery01.js"></script>
+        <script type="text/javascript" src="Template_files/jquery01.js"></script>
 
-		<!-- JQuery pager plugin script for tablesorter tables -->
-		<script type="text/javascript" src="Template_files/jquery02.js"></script>
+        <!-- JQuery pager plugin script for tablesorter tables -->
+        <script type="text/javascript" src="Template_files/jquery02.js"></script>
 
-		<!-- JQuery password strength plugin script -->
-		<script type="text/javascript" src="Template_files/jquery03.js"></script>
 
-		<!-- JQuery thickbox plugin script -->
-		<script type="text/javascript" src="Template_files/thickbox.js"></script>
+        <!-- JQuery password strength plugin script -->
+        <script type="text/javascript" src="Template_files/jquery03.js"></script>
+
+        <!-- JQuery thickbox plugin script -->
+        <script type="text/javascript" src="Template_files/thickbox.js"></script>
+        <script type="text/javascript" src="Template_files/jquery-1.5.min.js"></script>
+        <script type="text/javascript" src="Template_files/jquery.colorbox-min.js"></script>
+        <script type="text/javascript" src="../js/modules/exporting.js"></script>
+        <script type="text/javascript" src="../js/highcharts.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-1.8.14.custom.min.js"></script>
 
         <!-- Initiate WYIWYG text area -->
-		<script type="text/javascript">
-			$(function()
-			{
-			$('#wysiwyg').wysiwyg(
-			{
-			controls : {
-			separator01 : { visible : true },
-			separator03 : { visible : true },
-			separator04 : { visible : true },
-			separator00 : { visible : true },
-			separator07 : { visible : false },
-			separator02 : { visible : false },
-			separator08 : { visible : false },
-			insertOrderedList : { visible : true },
-			insertUnorderedList : { visible : true },
-			undo: { visible : true },
-			redo: { visible : true },
-			justifyLeft: { visible : true },
-			justifyCenter: { visible : true },
-			justifyRight: { visible : true },
-			justifyFull: { visible : true },
-			subscript: { visible : true },
-			superscript: { visible : true },
-			underline: { visible : true },
-            increaseFontSize : { visible : false },
-            decreaseFontSize : { visible : false }
-			}
-			} );
-			});
+        <script type="text/javascript">
+            $(function()
+            {
+                $('#wysiwyg').wysiwyg(
+                {
+                    controls : {
+                        separator01 : { visible : true },
+                        separator03 : { visible : true },
+                        separator04 : { visible : true },
+                        separator00 : { visible : true },
+                        separator07 : { visible : false },
+                        separator02 : { visible : false },
+                        separator08 : { visible : false },
+                        insertOrderedList : { visible : true },
+                        insertUnorderedList : { visible : true },
+                        undo: { visible : true },
+                        redo: { visible : true },
+                        justifyLeft: { visible : true },
+                        justifyCenter: { visible : true },
+                        justifyRight: { visible : true },
+                        justifyFull: { visible : true },
+                        subscript: { visible : true },
+                        superscript: { visible : true },
+                        underline: { visible : true },
+                        increaseFontSize : { visible : false },
+                        decreaseFontSize : { visible : false }
+                    }
+                } );
+            });
         </script>
 
         <!-- Initiate tablesorter script -->
+
         <script type="text/javascript">
-			$(document).ready(function() {
-				$("#myTable")
-				.tablesorter({
-					// zebra coloring
-					widgets: ['zebra'],
-					// pass the headers argument and assing a object
-					headers: {
-						// assign the sixth column (we start counting zero)
-						6: {
-							// disable it by setting the property sorter to false
-							sorter: false
-						}
-					}
-				})
-			.tablesorterPager({container: $("#pager")});
-		});
-		</script>
-
-        <!-- Initiate password strength script -->
-		<script type="text/javascript">
-			$(function() {
-			$('.password').pstrength();
-			});
+            $(document).ready(function() {
+                $("#myTable")
+                .tablesorter({
+                    // zebra coloring
+                    widgets: ['zebra'],
+                    // pass the headers argument and assing a object
+                    headers: {
+                        // assign the sixth column (we start counting zero)
+                        6: {
+                            // disable it by setting the property sorter to false
+                            sorter: false
+                        }
+                    }
+                })
+                .tablesorterPager({container: $("#pager")});
+            });
         </script>
-	</head>
-	<body>
-    	<!-- Header -->
+        <script type="text/javascript">
+            function tampilkan_profil_kk() {
+                $('#daftar_kk').hide();
+                $('#profil_kk').show();
+                return false;
+            };
+            function tampilkan_hasil_cari(){
+                $('#daftar_kk').show();
+                $('#profil_kk').hide();
+                return false;
+            };
+        </script>
+        <script>
+            $(function() {
+                $( "#datepicker" ).datepicker({
+                    changeMonth: true,
+                    changeYear: true
+                });
+            });
+        </script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $("#test").colorbox({initialHeight: "900px", initialWidth: "900px", width: "55%", height: "75%"})
+            });
+        </script>
+    </head>
+    <body>
         <div id="header">
-            <!-- Header. Status part -->
-            <div id="header-status">
-                <div class="container_12">
-                    <div class="grid_8">
-                        <span id="text-invitation"></span>
-                        <!-- Messages displayed through the thickbox -->
-
-                    </div>
-                    <div class="grid_4">
-                        <a href="" id="logout">
-                        Logout
-                        </a>
-                    </div>
-                </div>
-                <div style="clear:both;"></div>
-            </div> <!-- End #header-status -->
-        <img src="Template_files/logo0000.gif" style="position: absolute; top:56px; left:60px">
-           <img src="Template_files/puskesmas.gif" style="position: absolute; top:56px; left:120px">
-
             <!-- Header. Main part -->
             <div id="header-main">
                 <div class="container_12">
                     <div class="grid_12">
-                        
-                            <ul id="nav">
-                                <li><a href="index.php">Beranda</a></li>
-                                <li><a href="antrian.htm">Antrian</a></li>
-                                <li><a href="data_periksa.htm">Data Pemeriksaan</a></li>
-                                <li><a href="laporan.php">Laporan</a></li>
-			    </ul>
-
-                    </div><!-- End. .grid_12-->
-                    <div style="clear: both;"></div>
-                </div><!-- End. .container_12 -->
-            </div> <!-- End #header-main -->
-            <div style="clear: both;"></div>
-            <!-- Sub navigation -->
-            <div id="subnav">
-                <div class="container_12">
-                    <div class="grid_12">
-                        <ul>
-                             <li><a href="">Dinas</a></li>
-                            <li><a href="">Tahunan</a></li>
-                            <li><a href="">Bulanan</a></li>
-                            <li><a href="">Mingguan</a></li>
-                            <li><a href="">Harian</a></li>
+                        <img src="Template_files/logo0000.gif"/>
+                        <ul id="nav">
+                            <li  id="current"><a href="home_loket.php">Home</a></li>
+                            <li><a href="antrian.php">Antrian</a></li>
+                            <li><a href="registrasi.php">Registrasi</a></li>
+                            <li><a href="loket_pembayaran.php">Pembayaran</a></li>
+                            <li><a href="statistik.php">Statistik</a></li>
+                            <li><a href="laporan.php">Laporan</a></li>
                         </ul>
-
-                    </div><!-- End. .grid_12-->
-                </div><!-- End. .container_12 -->
-                <div style="clear: both;"></div>
-            </div> <!-- End #subnav -->
-        </div> <!-- End #header -->
+                    </div>
+                </div>
+            </div>
+        <div style="clear: both;"></div>
--- siemas/loket/registrasi.php
+++ siemas/loket/registrasi.php
@@ -1,193 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8;charset=utf-8" />
-        <title>Puskesmas Bogor Tengah</title>
-
-        <!-- CSS Reset -->
-        <link rel="stylesheet" type="text/css" href="Template_files/reset000.css" media="screen" />
-
-        <!-- Fluid 960 Grid System - CSS framework -->
-        <link rel="stylesheet" type="text/css" href="Template_files/grid0000.css" media="screen" />
-        <link rel="stylesheet" type="text/css" href="Template_files/colorbox.css" />
-        <link rel="stylesheet" type="text/css" href="css/redmond/jquery-ui-1.8.14.custom.css" />
-
-        <!-- IE Hacks for the Fluid 960 Grid System -->
-        <!--[if IE 6]><link rel="stylesheet" type="text/css" href="css/ie6.css" media="screen" /><![endif]-->
-        <!--[if IE 7]><link rel="stylesheet" type="text/css" href="css/ie.css" media="screen" /><![endif]-->
-
-        <!-- Main stylesheet -->
-        <link rel="stylesheet" type="text/css" href="Template_files/styles00.css" media="screen" />
-
-        <!-- WYSIWYG editor stylesheet -->
-        <link rel="stylesheet" type="text/css" href="Template_files/jquery00.css" media="screen" />
-
-        <!-- Table sorter stylesheet -->
-        <link rel="stylesheet" type="text/css" href="Template_files/tablesor.css" media="screen" />
-
-
-        <!-- Thickbox stylesheet -->
-        <link rel="stylesheet" type="text/css" href="Template_files/thickbox.css" media="screen" />
-
-        <!-- Themes. Below are several color themes. Uncomment the line of your choice to switch to different color. All styles commented out means blue theme. -->
-        <link rel="stylesheet" type="text/css" href="Template_files/theme-bl.css" media="screen" />
-        <!--<link rel="stylesheet" type="text/css" href="css/theme-red.css" media="screen" />-->
-        <!--<link rel="stylesheet" type="text/css" href="css/theme-yellow.css" media="screen" />-->
-        <!--<link rel="stylesheet" type="text/css" href="css/theme-green.css" media="screen" />-->
-        <!--<link rel="stylesheet" type="text/css" href="css/theme-graphite.css" media="screen" />-->
-
-        <!-- JQuery engine script-->
-
-        <script type="text/javascript" src="Template_files/jquery-1.js"></script>
-
-        <!-- JQuery WYSIWYG plugin script -->
-        <script type="text/javascript" src="Template_files/jquery00.js"></script>
-
-        <!-- JQuery tablesorter plugin script-->
-        <script type="text/javascript" src="Template_files/jquery01.js"></script>
-
-        <!-- JQuery pager plugin script for tablesorter tables -->
-        <script type="text/javascript" src="Template_files/jquery02.js"></script>
-
-
-        <!-- JQuery password strength plugin script -->
-        <script type="text/javascript" src="Template_files/jquery03.js"></script>
-
-        <!-- JQuery thickbox plugin script -->
-        <script type="text/javascript" src="Template_files/thickbox.js"></script>
-        <script type="text/javascript" src="Template_files/jquery-1.5.min.js"></script>
-        <script type="text/javascript" src="Template_files/jquery.colorbox-min.js"></script>
-        <script type="text/javascript" src="js/jquery-ui-1.8.14.custom.min.js"></script>
-
-        <!-- Initiate WYIWYG text area -->
-        <script type="text/javascript">
-            $(function()
-            {
-                $('#wysiwyg').wysiwyg(
-                {
-                    controls : {
-                        separator01 : { visible : true },
-                        separator03 : { visible : true },
-                        separator04 : { visible : true },
-                        separator00 : { visible : true },
-                        separator07 : { visible : false },
-                        separator02 : { visible : false },
-                        separator08 : { visible : false },
-                        insertOrderedList : { visible : true },
-                        insertUnorderedList : { visible : true },
-                        undo: { visible : true },
-                        redo: { visible : true },
-                        justifyLeft: { visible : true },
-                        justifyCenter: { visible : true },
-                        justifyRight: { visible : true },
-                        justifyFull: { visible : true },
-                        subscript: { visible : true },
-                        superscript: { visible : true },
-                        underline: { visible : true },
-                        increaseFontSize : { visible : false },
-                        decreaseFontSize : { visible : false }
-                    }
-                } );
-            });
-        </script>
-        <!-- Initiate tablesorter script -->
-
-        <script type="text/javascript">
-            $(document).ready(function() {
-                $("#myTable")
-                .tablesorter({
-                    // zebra coloring
-                    widgets: ['zebra'],
-                    // pass the headers argument and assing a object
-                    headers: {
-                        // assign the sixth column (we start counting zero)
-                        6: {
-                            // disable it by setting the property sorter to false
-                            sorter: false
-                        }
-                    }
-                })
-                .tablesorterPager({container: $("#pager")});
-            });
-        </script>
-
-        <!-- Initiate password strength script -->
-        <script type="text/javascript">
-            $(function() {
-                $('.password').pstrength();
-            });
-        </script>
-        <script type="text/javascript">
-            function tampilkan_profil_kk() {
-                $('#daftar_kk').hide();
-                $('#profil_kk').show();
-                return false;
-            };
-            function tampilkan_hasil_cari(){
-                $('#daftar_kk').show();
-                $('#profil_kk').hide();
-                return false;
-            };
-        </script>
-
-        <div id="header">
-            <!-- Header. Main part -->
-            <div id="header-main">
+<?php include 'header.php';?>            
+<div id="subnav">
                 <div class="container_12">
                     <div class="grid_12">
-                        <img src="Template_files/logo0000.gif"/>
-                        <ul id="nav">
-                            <li><a href="">Home</a></li>
-                            <li><a href="antrian.htm">Antrian</a></li>
-                            <li><a href="data_pasien.htm">Pasien</a></li>
-                            <li><a href="">Obat</a></li>
-                            <li><a href="">Statistik</a></li>
-                            <li id="current"><a href="laporan_harian.htm">Laporan</a></li>
-                        </ul>
-
-                    </div><!-- End. .grid_12-->
-                    <div style="clear: both;"></div>
+                        
+                    </div>
                 </div>
-                <!-- End. .container_12 -->
-            </div>
-            <!-- End #header-main -->
-            <div style="clear: both;"></div>
-            <!-- Sub navigation -->
-            <div id="subnav">
-                <div class="container_12">
-                    <div class="grid_12">
-                        <ul>
-                            <li><a href="">Dinas</a></li>
-                            <li><a href="">Tahunan</a></li>
-                            <li><a href="">Bulanan</a></li>
-                            <li><a href="">Mingguan</a></li>
-                            <li><a href="">Harian</a></li>
-                        </ul>
-
-                    </div><!-- End. .grid_12-->
-                </div><!-- End. .container_12 -->
                 <div style="clear: both;"></div>
             </div>
-            <!-- End #subnav -->
-        </div>
-
-
-    </head>
-
-    <body>
-        <script>
-            $(function() {
-                $( "#datepicker" ).datepicker({
-                    changeMonth: true,
-                    changeYear: true
-                });
-            });
-        </script>
-        <script type="text/javascript">
-            $(document).ready(function(){
-                $("#test").colorbox({initialHeight: "900px", initialWidth: "900px", width: "55%", height: "75%"})
-            });
-        </script>
+</div>
         <div>
             <!-- KIRI -->
             <div class="grid_6" style="width: 45%">
@@ -273,9 +93,7 @@
                     </div>
                 </div>
             </div>
-        </div>
-
-        <!-- KANAN -->
+            <!-- KANAN -->
         <div class="grid_6" style="width: 49%">
             <div class="module">
                 <h2><span>Registrasi KK Baru</span></h2>
@@ -347,8 +165,7 @@
                 </div>
             </div>
         </div>
+        </div>
         <div style="clear: both;"></div>
-
     </body>
-
 </html>
