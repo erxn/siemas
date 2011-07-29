b59836dd69d9bffb2d8bb747856d322c6d17638c
annisa.anastasia <annisa.anastasia@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-07-29 07:49:14 +0000

    komit kelimabelas
    hasil pencarian belum
    registrasi beres

--- siemas/loket/system/application/models/m_pasien.php
+++ siemas/loket/system/application/models/m_pasien.php
@@ -10,10 +10,8 @@ class M_pasien extends Model {
         $data = array();
         $q = $this->db->query("SELECT * FROM pasien");
 
-        if($q->num_rows() > 0)
-        {
-            foreach ($q->result_array() as $row)
-            {
+        if($q->num_rows() > 0) {
+            foreach ($q->result_array() as $row) {
                 $data[] = $row;
             }
         }
@@ -22,10 +20,10 @@ class M_pasien extends Model {
         return $data;
     }
 
-    function insert_data_pasien($data){
+    function insert_data_pasien($data) {
         $insert = $this->db->insert('pasien',$data);
 
-        if($insert){
+        if($insert) {
             return $this->db->insert_id(); //fungsi dari CInya
         }
         else {
@@ -33,14 +31,12 @@ class M_pasien extends Model {
         }
 
     }
-    function lihat_profil_pasien($id_kk, $id_pasien){
+    function lihat_profil_pasien($id_kk, $id_pasien) {
         $data = array();
         $q = $this->db->query("SELECT pasien.*, kk.* FROM pasien JOIN kk USING(id_kk) WHERE pasien.id_pasien = $id_pasien AND pasien.id_kk = $id_kk");
 
-        if($q->num_rows() > 0)
-        {
-            foreach ($q->result_array() as $row)
-            {
+        if($q->num_rows() > 0) {
+            foreach ($q->result_array() as $row) {
                 $data[] = $row;
             }
         }
@@ -49,8 +45,23 @@ class M_pasien extends Model {
         return $data;
     }
 
-    function tambah_id_pasien($id_kk){
-        $jumlah = $this->db->query("SELECT COUNT(id_kk) FROM pasien WHERE id_kk =  $id_kk");
-        echo jumlah;
+    function tambah_id_pasien($id_kk) {
+        $q = $this->db->query("SELECT COUNT(id_kk) AS jumlah FROM pasien WHERE id_kk =  $id_kk");
+        $jumlah = $q->result_array();
+        return $jumlah[0]['jumlah'];
     }
-}
+
+    function cari_pasien($id_pasien,$nama_pasien,$umur_pasien) {
+        if($id_pasien) {
+            $q = $this->db->query("SELECT extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) AS umur, *
+                                 FROM pasien
+                                 WHERE id_pasien=$id_pasien");
+        }
+            else if($nama_pasien != '' && $umur_pasien != '') {
+                $q = $this->db->query("SELECT *, extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) AS umur
+                             FROM pasien
+                             WHERE extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) BETWEEN $umur_pasien-1 and $umur_pasien+1
+                                   AND nama_pasien LIKE '%$nama_pasien%'");
+            }
+        }
+    }
