77d860aa1b69971e71758392fbf9b779e1bd12d8
annisa.anastasia <annisa.anastasia@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-08-02 23:14:17 +0000

    komit kesembilan
    data pasien udah bisa dicari
    
    masih gabisa
    rincian biaya harus dibenerin lagi
    tambahin lagi jenis pelayanan biar bisa sesuai sama pelayanannya

--- siemas/loket/system/application/views/pilih_poli_pasien_lama.php
+++ siemas/loket/system/application/views/pilih_poli_pasien_lama.php
@@ -15,12 +15,13 @@
     }
 </style>
 
-<div style="border: 0px solid fuchsia; width: 400px; height: auto;padding: 2px; text-align: center; margin: 0 auto">
+<div style="border: 0px solid fuchsia; width: 700px; height: auto;padding: 2px; text-align: center; margin: 0 auto">
     <a href="#" onclick="tambah_antrian('umum'); return false;" class="kotak"><img src="images/umum.png" border="0"/><br/>Poli Umum</a>
     <a href="#" onclick="tambah_antrian('gigi'); return false;" class="kotak"><img src="images/gigi.png" border="0"/><br/>Poli Gigi</a>
     <a href="#" onclick="tambah_antrian('kia'); return false;" class="kotak"><img src="images/kia.png" border="0"/><br/>Poli KIA</a>
     <a href="#" onclick="tambah_antrian('lab'); return false;" class="kotak"><img src="images/lab.png" border="0"/><br/>Laboratorium</a>
     <a href="#" onclick="tambah_antrian('radiologi'); return false;" class="kotak"><img src="images/radiologi.jpg" border="0"/><br/>Radiologi</a>
+    <a href="#" onclick="tambah_antrian('rujukan'); return false;" class="kotak"><img src="images/radiologi.jpg" border="0"/><br/>Rujukan</a>
 </div>
 
 <script type="text/javascript">
