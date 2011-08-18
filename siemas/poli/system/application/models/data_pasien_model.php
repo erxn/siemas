d93ef2b96524da71678d3a5f89f96882f2381fc3
meri.marlina20@gmail.com <meri.marlina20@gmail.com@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-08-18 03:03:47 +0000

    gigimodel

--- siemas/poli/system/application/models/data_pasien_model.php
+++ siemas/poli/system/application/models/data_pasien_model.php
@@ -10,7 +10,46 @@ Class Data_pasien_model extends Model{
     function Data_pasien_model (){
         parent::Model();
     }
-    
+
+    function cari_pasien($kode_pasien,$nama_pasien,$umur_pasien,$alamat="") {
+
+        $where = "WHERE 1 ";
+
+        if($nama_pasien != "") $where .= " AND nama_pasien LIKE '%$nama_pasien%'";
+        if($umur_pasien != "") $where .= " AND extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) BETWEEN $umur_pasien-1 and $umur_pasien+1";
+        if($alamat != "") $where .= "AND alamat_kk LIKE '%$alamat_pasien%'";
+
+        $data = array();
+        if($kode_pasien != '') {
+            $q = $this->db->query("SELECT pasien.*, kk.*, extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) AS umur
+                                 FROM pasien JOIN kk USING(id_kk)
+                                 WHERE kode_pasien = '$kode_pasien'");
+            if($q->num_rows() > 0) {
+                foreach ($q->result_array() as $row) {
+                    $data[] = $row;
+                }
+            }
+            $q->free_result();
+            return $data;
+
+        }
+        else {
+
+            $q = $this->db->query("SELECT pasien.*, kk.*, extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) AS umur
+                                 FROM pasien JOIN kk USING(id_kk) $where");
+
+
+            if($q->num_rows() > 0) {
+                foreach ($q->result_array() as $row) {
+                    $data[] = $row;
+                }
+            }
+
+            $q->free_result();
+            return $data;
+        }
+
+    }
     function get_semua_data_pasien(){                        //buat ngeluarin semua data pasiennnn        
         $data=array();
         $kueri=$this->db->query("SELECT * FROM pasien JOIN kk" );
--- siemas/poli/system/application/models/laporan_model.php
+++ siemas/poli/system/application/models/laporan_model.php
@@ -9,41 +9,52 @@ Class Laporan_model extends Model{
     function Laporan_model(){
         parent::Model();
     }
-     function lap_hp(){                       
-        $data=array();
-        $kueri=$this->db->query("SELECT * FROM  kk
-                        JOIN pasien
-                        ON kk.id_KK=pasien.id_KK
-                        JOIN remed_poli_gigi
-                        ON pasien.id_pasien=remed_poli_gigi.id_pasien
-                        JOIN penyakit_remed_gigi
-                        ON remed_poli_gigi.id_remed_gigi=penyakit_remed_gigi.id_remed_gigi
-                        JOIN penyakit
-                        ON penyakit_remed_gigi.id_penyakit=penyakit.id_penyakit
-                                " );
 
+    function layanan_harian($tanggal, $bulan, $tahun){
+         $data=array();
+
+         $tgl = date("Y-m-d", strtotime("$tahun-$bulan-$tanggal"));
+
+        $kueri=$this->db->query("SELECT COUNT(l.id_layanan) as jumlah, l.id_layanan
+                                 FROM remed_poli_gigi r
+                                 JOIN remed_gigi_layanan l USING (id_remed_gigi)
+                                 WHERE r.tanggal_kunjungan_gigi = '$tgl'
+                                 GROUP BY l.id_layanan" );
         if($kueri->num_rows()>0){
             foreach ($kueri->result_array()as $row){
                 $data[]=$row;
             }
         }
         $kueri->free_result();
-        return $data;
+
+        // isi yg kosong
+
+        $min = 2;
+        $max = 20;
+
+        $column1 = array();
+        foreach ($data as $row) $column1[] = $row['id_layanan'];
+
+        $column2 = array();
+        for ($i = $min; $i <= $max; $i++) $column2[] = $i;
+
+  	$emptys = array_merge(array_diff($column1, $column2), array_diff($column2, $column1));
+	$empty_arr = array();
+
+	foreach ($emptys as $e) {
+            $empty_arr[] = array('id_layanan' => $e, 'jumlah' => 0);
+	}
+
+	$res = array_merge($data, $empty_arr);
+	sort($res);
+
+	return $res;
+        
     }
 
-function lap_hp_excell(){                      
-
-        $data=array();
-        $kueri=$this->db->query("SELECT * FROM  kk
-                        JOIN pasien
-                        ON kk.id_KK=pasien.id_KK
-                        JOIN remed_poli_gigi
-                        ON pasien.id_pasien=remed_poli_gigi.id_pasien
-                        JOIN penyakit_remed_gigi
-                        ON remed_poli_gigi.id_remed_gigi=penyakit_remed_gigi.id_remed_gigi
-                        JOIN penyakit
-                        ON penyakit_remed_gigi.id_penyakit=penyakit.id_penyakit" );
-
+    function layanan(){
+         $data=array();
+        $kueri=$this->db->query("SELECT * FROM layanan" );
         if($kueri->num_rows()>0){
             foreach ($kueri->result_array()as $row){
                 $data[]=$row;
@@ -51,56 +62,121 @@ function lap_hp_excell(){
         }
         $kueri->free_result();
         return $data;
+
     }
 
+    function penyakit_harian($tanggal, $bulan, $tahun){
+         $data=array();
 
+         $tgl = date("Y-m-d", strtotime("$tahun-$bulan-$tanggal"));
 
-
-     function lap_ht(){
-        $data=array();
-        $kueri=$this->db->query("SELECT * FROM  kk
-                        JOIN pasien
-                        ON kk.id_KK=pasien.id_KK
-                        JOIN remed_poli_gigi
-                        ON pasien.id_pasien=remed_poli_gigi.id_pasien
-                        JOIN remed_gigi_layanan
-                        ON remed_poli_gigi.id_remed_gigi=remed_gigi_layanan.id_remed_gigi
-                        JOIN layanan
-                        ON remed_gigi_layanan.id_layanan=layanan.id_layanan
-                                " );
-
+        $kueri=$this->db->query("SELECT COUNT(l.id_penyakit) as jumlah, l.id_penyakit, 
+                extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) BETWEEN 1 and 4, p.jk_pasien,
+                                 FROM pasien p
+                                 JOIN   remed_poli_gigi r USING (id_pasien)
+                                 JOIN penyakit_remed_gigi l USING (id_remed_gigi)
+                                 WHERE r.tanggal_kunjungan_gigi = '$tgl'
+                                 GROUP BY l.id_penyakit" );
         if($kueri->num_rows()>0){
             foreach ($kueri->result_array()as $row){
                 $data[]=$row;
             }
         }
         $kueri->free_result();
-        return $data;
+
+        // isi yg kosong
+
+        $min = 2;
+        $max = 20;
+
+        $column1 = array();
+        foreach ($data as $row) $column1[] = $row['id_penyakit'];
+
+        $column2 = array();
+        for ($i = $min; $i <= $max; $i++) $column2[] = $i;
+
+  	$emptys = array_merge(array_diff($column1, $column2), array_diff($column2, $column1));
+	$empty_arr = array();
+
+	foreach ($emptys as $e) {
+            $empty_arr[] = array('id_penyakit' => $e, 'jumlah' => 0);
+	}
+
+	$res = array_merge($data, $empty_arr);
+	sort($res);
+
+	return $res;
+
     }
 
-function lap_ht_excell(){
+    function get_data_laporan_bulanan_penyakit($tanggal, $bulan, $tahun,  $umur_min, $umur_max,$id_penyakit) {
 
-        $data=array();
-        $kueri=$this->db->query("SELECT * FROM  kk
-                        JOIN pasien
-                        ON kk.id_KK=pasien.id_KK
-                        JOIN remed_poli_gigi
-                        ON pasien.id_pasien=remed_poli_gigi.id_pasien
-                        JOIN remed_gigi_layanan
-                        ON remed_poli_gigi.id_remed_gigi=remed_gigi_layanan.id_remed_gigi
-                        JOIN layanan
-                        ON remed_gigi_layanan.id_layanan=layanan.id_layanan" );
+        $tgl = date("Y-m-d", strtotime("$tahun-$bulan-$tanggal"));
+        $kueri = $this->db->query("SELECT
+                   (SELECT COUNT(*)
+                    FROM
+                      (
+                       SELECT pasien.*, extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) AS umur,
+                              IF(tanggal_pendaftaran <= '$tgl', 1, 0) AS baru
+                       FROM remed_poli_gigi r
+                       JOIN penyakit_remed_gigi p USING (id_remed_gigi)
+                       JOIN pasien USING (id_pasien)
+                       WHERE r.tanggal_kunjungan_gigi= '$tgl' AND p.id_penyakit = $id_penyakit
+                      ) AS p
+                    WHERE p.umur BETWEEN $umur_min AND $umur_max
+                      AND p.jk_pasien = 'Perempuan'
+                      AND p.baru = 1
+                    ) AS a,
+                   (SELECT COUNT(*)
+                    FROM
+                      (
+                       SELECT pasien.*, extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) AS umur,
+                              IF(tanggal_pendaftaran <= '$tgl', 1, 0) AS baru
+                       FROM remed_poli_gigi r
+                       JOIN penyakit_remed_gigi p USING (id_remed_gigi)
+                       JOIN pasien USING (id_pasien)
+                       WHERE r.tanggal_kunjungan_gigi= '$tgl' AND p.id_penyakit = $id_penyakit
+                      ) AS p
+                    WHERE p.umur BETWEEN $umur_min AND $umur_max
+                      AND p.jk_pasien = 'Perempuan'
+                      AND p.baru = 0
+                    ) AS b,
+                   (SELECT COUNT(*)
+                    FROM
+                      (
+                       SELECT pasien.*, extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) AS umur,
+                              IF(tanggal_pendaftaran <= '$tgl', 1, 0) AS baru
+                       FROM remed_poli_gigi r
+                       JOIN penyakit_remed_gigi p USING (id_remed_gigi)
+                       JOIN pasien USING (id_pasien)
+                       WHERE r.tanggal_kunjungan_gigi= '$tgl' AND p.id_penyakit =$id_penyakit
+                      ) AS p
+                    WHERE p.umur BETWEEN $umur_min AND $umur_max
+                      AND p.jk_pasien = 'Laki-laki'
+                      AND p.baru = 1
+                    ) AS c,
+                   (SELECT COUNT(*)
+                    FROM
+                      (
+                       SELECT pasien.*, extract(YEAR FROM from_days(datediff(curdate(), tanggal_lahir))) AS umur,
+                              IF(tanggal_pendaftaran <= '$tgl', 1, 0) AS baru
+                       FROM remed_poli_gigi r
+                       JOIN penyakit_remed_gigi p USING (id_remed_gigi)
+                       JOIN pasien USING (id_pasien)
+                       WHERE r.tanggal_kunjungan_gigi= '$tgl' AND p.id_penyakit = $id_penyakit
+                      ) AS p
+                    WHERE p.umur BETWEEN $umur_min AND $umur_max
+                      AND p.jk_pasien = 'Laki-laki'
+                      AND p.baru = 0
+                    ) AS d");
 
-        if($kueri->num_rows()>0){
-            foreach ($kueri->result_array()as $row){
-                $data[]=$row;
-            }
+         if($kueri->num_rows()>0){
+            $data = $kueri->row_array();
         }
         $kueri->free_result();
         return $data;
+
     }
 
 
 }
-
-?>
