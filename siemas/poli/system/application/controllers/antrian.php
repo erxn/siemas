a19fd3f42d4a280f612bf520d52d68b97f1695eb
annisa.anastasia@gmail.com <annisa.anastasia@gmail.com@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-08-20 08:17:41 +0000

    meri,,baru

--- siemas/poli/system/application/controllers/antrian.php
+++ siemas/poli/system/application/controllers/antrian.php
@@ -17,21 +17,44 @@ class Antrian extends Controller{
 
     }
 
-     function antri($id_poli){
-        $ant=$this->antrian->tabel_antrian_a($id_poli);
-        $data['a']=$ant;
-
-        $ant=$this->antrian->tabel_antrian_sp($id_poli);
-        $data['s']=$ant;
-        $this->load->view('antrian_view',$data);
+    function antri(){
+        $this->load->view('antrian_view');
     }
 
+    function tabel_antri($id_poli) {
+        $ant=$this->antrian->tabel_antrian_a($id_poli);
+        $data['a']=$ant;
+        $this->load->view('antrian_tabel_antri_view', $data);
+    }
 
+    function tabel_diperiksa($id_poli) {
+        $ant=$this->antrian->tabel_antrian_sp($id_poli);
+        $data['s']=$ant;
+        $this->load->view('antrian_tabel_diperiksa_view', $data);
+    }
+
+    function tabel_tunda($id_poli) {
+        $ant=$this->antrian->tabel_antrian_tunda($id_poli);
+        $data['t']=$ant;
+        $this->load->view('antrian_tabel_tunda_view', $data);
+    }
 
     function isi_remed_hari_ini($id_poli){
         $antrian=$this->antrian->tabel_antrian_selesai($id_poli);
         $data1['selesai']=$antrian;
         $this->load->view('isi_remed',$data1);
+    }
+
+    function periksa($id_antrian) {
+        $this->antrian->ubah_status($id_antrian, 'SEDANG DIPROSES');
+    }
+
+    function selesai($id_antrian) {
+        $this->antrian->ubah_status($id_antrian, 'SELESAI');
+    }
+
+    function lewati($id_antrian) {
+        $this->antrian->ubah_status($id_antrian, 'TUNDA');
     }
 }
 
--- siemas/poli/system/application/models/antrian_m.php
+++ siemas/poli/system/application/models/antrian_m.php
@@ -2,61 +2,105 @@
 /* 
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
- */
+*/
 
-class Antrian_m extends Model{
-    function Antrian_m(){
+class Antrian_m extends Model {
+    function Antrian_m() {
         parent::Model();
     }
 
-    function tabel_antrian_a($id_poli){
-          $data=array();
-        $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien
+    function tabel_antrian_a($id_poli) {
+        $data=array();
+        $now = date("Y-m-d");
+
+        $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien, extract(YEAR FROM from_days(datediff(curdate(), pasien.tanggal_lahir))) AS umur
                                 FROM antrian
                                 JOIN kunjungan USING (id_kunjungan)
                                 JOIN pasien USING (id_pasien)
-                                WHERE antrian.id_poli = $id_poli AND antrian.status= 'ANTRI'" );
+                                WHERE 
+                                    antrian.id_poli = $id_poli
+                                    AND antrian.status= 'ANTRI'
+                                    AND kunjungan.tanggal_kunjungan = '$now'
+                                ORDER BY kunjungan.no_kunjungan" );
 
-        if($kueri->num_rows()>0){
-            foreach ($kueri->result_array()as $row){
+        if($kueri->num_rows()>0) {
+            foreach ($kueri->result_array()as $row) {
                 $data[]=$row;
             }
         }
         $kueri->free_result();
-         return $data;
+        return $data;
     }
-    function tabel_antrian_sp($id_poli){
-          $data=array();
-        $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien
+    function tabel_antrian_sp($id_poli) {
+        $data=array();
+        $now = date("Y-m-d");
+        $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien, extract(YEAR FROM from_days(datediff(curdate(), pasien.tanggal_lahir))) AS umur
                                 FROM antrian
                                 JOIN kunjungan USING (id_kunjungan)
                                 JOIN pasien USING (id_pasien)
-                                WHERE antrian.id_poli = $id_poli AND antrian.status='SEDANG DIPROSES'" );
+                                WHERE
+                                    antrian.id_poli = $id_poli
+                                    AND antrian.status= 'SEDANG DIPROSES'
+                                    AND kunjungan.tanggal_kunjungan = '$now'
+                                ORDER BY kunjungan.no_kunjungan" );
 
-        if($kueri->num_rows()>0){
-            foreach ($kueri->result_array()as $row){
+        if($kueri->num_rows()>0) {
+            foreach ($kueri->result_array()as $row) {
                 $data[]=$row;
             }
         }
         $kueri->free_result();
-         return $data;
+        return $data;
     }
 
-    function tabel_antrian_selesai($id_poli){
-          $data=array();
-        $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien
+    function tabel_antrian_selesai($id_poli) {
+        $data=array();
+        $now = date("Y-m-d");
+        $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien, extract(YEAR FROM from_days(datediff(curdate(), pasien.tanggal_lahir))) AS umur
                                 FROM antrian
                                 JOIN kunjungan USING (id_kunjungan)
                                 JOIN pasien USING (id_pasien)
-                                WHERE antrian.id_poli =$id_poli AND antrian.status='SELESAI'" );
+                                WHERE
+                                    antrian.id_poli = $id_poli
+                                    AND antrian.status= 'SELESAI'
+                                    AND kunjungan.tanggal_kunjungan = '$now'
+                                ORDER BY kunjungan.no_kunjungan" );
 
-        if($kueri->num_rows()>0){
-            foreach ($kueri->result_array()as $row){
+        if($kueri->num_rows()>0) {
+            foreach ($kueri->result_array()as $row) {
                 $data[]=$row;
             }
         }
         $kueri->free_result();
-         return $data;
+        return $data;
+    }
+
+    function tabel_antrian_tunda($id_poli) {
+        $data=array();
+        $now = date("Y-m-d");
+        $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien, extract(YEAR FROM from_days(datediff(curdate(), pasien.tanggal_lahir))) AS umur
+                                FROM antrian
+                                JOIN kunjungan USING (id_kunjungan)
+                                JOIN pasien USING (id_pasien)
+                                WHERE
+                                    antrian.id_poli = $id_poli
+                                    AND antrian.status= 'TUNDA'
+                                    AND kunjungan.tanggal_kunjungan = '$now'
+                                ORDER BY kunjungan.no_kunjungan" );
+
+        if($kueri->num_rows()>0) {
+            foreach ($kueri->result_array()as $row) {
+                $data[]=$row;
+            }
+        }
+        $kueri->free_result();
+        return $data;
+    }
+
+    function ubah_status($id_antrian, $status) {
+
+        $this->db->query("UPDATE antrian SET status = '$status' WHERE id_antrian = $id_antrian");
+
     }
 }
 
