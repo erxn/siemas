87c7bdbd7c1b82d54054394d760eba83b8cc4faf
meri.marlina20@gmail.com <meri.marlina20@gmail.com@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-08-20 04:36:41 +0000

    model

--- siemas/poli/system/application/models/antrian_m.php
+++ siemas/poli/system/application/models/antrian_m.php
@@ -9,13 +9,46 @@ class Antrian_m extends Model{
         parent::Model();
     }
 
-    function tabel_antrian($id_poli){
+    function tabel_antrian_a($id_poli){
           $data=array();
         $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien
                                 FROM antrian
                                 JOIN kunjungan USING (id_kunjungan)
                                 JOIN pasien USING (id_pasien)
-                                WHERE antrian.id_poli = $id_poli" );
+                                WHERE antrian.id_poli = $id_poli AND antrian.status= 'ANTRI'" );
+
+        if($kueri->num_rows()>0){
+            foreach ($kueri->result_array()as $row){
+                $data[]=$row;
+            }
+        }
+        $kueri->free_result();
+         return $data;
+    }
+    function tabel_antrian_sp($id_poli){
+          $data=array();
+        $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien
+                                FROM antrian
+                                JOIN kunjungan USING (id_kunjungan)
+                                JOIN pasien USING (id_pasien)
+                                WHERE antrian.id_poli = $id_poli AND antrian.status='SEDANG DIPROSES'" );
+
+        if($kueri->num_rows()>0){
+            foreach ($kueri->result_array()as $row){
+                $data[]=$row;
+            }
+        }
+        $kueri->free_result();
+         return $data;
+    }
+
+    function tabel_antrian_selesai($id_poli){
+          $data=array();
+        $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien
+                                FROM antrian
+                                JOIN kunjungan USING (id_kunjungan)
+                                JOIN pasien USING (id_pasien)
+                                WHERE antrian.id_poli =$id_poli AND antrian.status='SELESAI'" );
 
         if($kueri->num_rows()>0){
             foreach ($kueri->result_array()as $row){
