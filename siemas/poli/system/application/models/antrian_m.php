9c5ab70638554cc7ef2da8718e032e56b6a237f9
meri.marlina20 <meri.marlina20@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-08-21 15:26:54 +0000

    model

--- siemas/poli/system/application/models/antrian_m.php
+++ siemas/poli/system/application/models/antrian_m.php
@@ -97,11 +97,45 @@ class Antrian_m extends Model {
         return $data;
     }
 
-    function ubah_status($id_antrian, $status) {
+    function tabel_antrian_terisi($id_poli) {
+        $data=array();
+        $now = date("Y-m-d");
+        $kueri=$this->db->query("SELECT antrian.*, kunjungan.*, pasien.nama_pasien, pasien.jk_pasien, extract(YEAR FROM from_days(datediff(curdate(), pasien.tanggal_lahir))) AS umur
+                                FROM antrian
+                                JOIN kunjungan USING (id_kunjungan)
+                                JOIN pasien USING (id_pasien)
+                                WHERE
+                                    antrian.id_poli = $id_poli
+                                    AND antrian.status= 'TERISI'
+                                    AND kunjungan.tanggal_kunjungan = '$now'
+                                ORDER BY kunjungan.no_kunjungan" );
 
+        if($kueri->num_rows()>0) {
+            foreach ($kueri->result_array()as $row) {
+                $data[]=$row;
+            }
+        }
+        $kueri->free_result();
+        return $data;
+    }
+
+    function ubah_status($id_antrian, $status) {
         $this->db->query("UPDATE antrian SET status = '$status' WHERE id_antrian = $id_antrian");
 
     }
-}
 
+    function data_pasien_remed($id_pasien){                               //buat nampilin data pasien di database di tampilan remed
+         $data=array();
+        $q=$this->db->query("SELECT * FROM pasien WHERE id_pasien=$id_pasien" );
+
+        if($q->num_rows()>0){
+            foreach ($q->result_array()as $row){
+                $data[]=$row;
+            }
+        }
+        $q->free_result();
+        return $data;
+    }
+
+}
 ?>
