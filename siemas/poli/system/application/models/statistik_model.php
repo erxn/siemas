865fd7fc62eecad035b5f95ec299ffc721ce25ce
meri.marlina20@gmail.com <meri.marlina20@gmail.com@804b9fc8-0f2b-9655-2ba3-8d792a735ee1>
2011-08-25 06:24:46 +0000

    controller

--- siemas/poli/system/application/models/statistik_model.php
+++ siemas/poli/system/application/models/statistik_model.php
@@ -21,18 +21,32 @@ Class Statistik_model extends Model {
         return $data['jumlah'];
     }
 
-    function tahunan_penyakit($tahun, $id_penyakit) {
+    function tahunan_penyakit($bulan, $tahun, $id_penyakit) {
         $data=array();
         $q=$this->db->query("SELECT COUNT(g.id_pasien) as jumlah
                              FROM penyakit AS p
                              JOIN penyakit_remed_gigi AS r USING(id_penyakit)
                              JOIN remed_poli_gigi AS g USING(id_remed_gigi)
-                             WHERE p.kode_penyakit=$id_penyakit AND YEAR(g.tanggal_kunjungan_gigi)=$tahun");
+                             WHERE p.kode_penyakit=$id_penyakit AND  MONTH(g.tanggal_kunjungan_gigi) = $bulan AND YEAR(g.tanggal_kunjungan_gigi)=$tahun");
 
         $data = $q->row_array();
 
         $q->free_result();
-        return $data['jumlah'];
+        return $data;
+    }
+
+    function get_id_by_penyakit($penyakit){
+         $data="";
+          $data_pasien=$this->db->query("
+                                SELECT id_penyakit
+                                FROM penyakit
+                                WHERE nama_penyakit LIKE '%$penyakit%'");
+
+          if($data_pasien->num_rows()>0){
+              $data=$data_pasien->row();
+          }
+           $data_pasien->free_result();
+           return $data;
     }
 
 }
